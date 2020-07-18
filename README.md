44639ef8fc0fcaf330ca0f6df1aed81464ecea4e
jaaybe <jb@motionmusic.us>
2020-07-18 16:23:00 -0700

    move from develop to master

--- README.md
+++ README.md
@@ -1,3 +1,60 @@
-# Professional README Generator Starter Code
 
-[How to create a Professional README](./readme-guide.md)
+# TEST
+
+## Description
+
+new test new test new test  
+Technologies used:
+HTML,CSS,JavaScript,3rd Party API,JS Node,SQL
+
+
+## Table of Contents
+
+* [Installation](#installation)
+* [Usage](#usage)
+* [Credits](#credits)
+* [License](#license)
+* [Contributing](#contributing)
+* [Tests](#tests)
+* [Questions](#questions)
+
+
+## Installation
+
+install it the best way you know how 
+
+
+## Usage 
+
+use it well 
+
+
+## Credits
+
+JB and friends
+
+
+## License
+
+Creative Commons Zero v1.0 Universal
+
+![GitHub license](https://img.shields.io/badge/license-Creative-blue.svg)
+
+
+## Contributing
+
+none. The [Contributor Covenant](https://www.contributor-covenant.org/) is an industry standard, but you can always write your own.
+
+
+## Tests
+
+nope
+
+       
+## Questions
+
+If you have any questions, please email JB Blot at: jb@m.com
+
+If you'd like to clone or explore the app repository, it is available on GitHub:
+https://github.com/jaaybe/
+    
---
+++ src/page-template.js
@@ -0,0 +1,66 @@
+
+module.exports = answers => {
+    // console.log(answers);
+const { title, description, installation, usage, credits, license, contributing, tests, tech, github, email } = answers;
+
+    return `
+# ${title}
+
+## Description
+${description} 
+Technologies used:
+${tech}
+
+
+## Table of Contents
+
+* [Installation](#installation)
+* [Usage](#usage)
+* [Credits](#credits)
+* [License](#license)
+* [Contributing](#contributing)
+* [Tests](#tests)
+* [Questions](#questions)
+
+
+## Installation
+
+${installation}
+
+
+## Usage 
+
+${usage} 
+
+
+## Credits
+
+${credits}
+
+
+## License
+
+${license}
+
+![GitHub license](https://img.shields.io/badge/license-${license.split(' ')[0]}-blue.svg)
+
+
+## Contributing
+
+${contributing}. The [Contributor Covenant](https://www.contributor-covenant.org/) is an industry standard, but you can always write your own.
+
+
+## Tests
+
+${tests}
+
+       
+## Questions
+
+If you have any questions, please email JB Blot at: ${email}
+
+If you'd like to clone or explore the app repository, it is available on GitHub:
+https://github.com/${github}/
+    `;
+
+};
