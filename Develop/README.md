0f0525e4ef068a5a9cd9ac82298e799add37a781
jaaybe <jb@motionmusic.us>
2020-07-18 16:13:31 -0700

    page-template completed

---
+++ Develop/README.md
@@ -0,0 +1,60 @@
+
+# TEST APPLICATION
+
+## Description
+
+sldkfj sldkfj sldkfj sldkfj sldkfj sldkfj sldkfj sldkfj sldkfj slkdfj sldkjfslkfj slkdjf slkjdf slkdjf slkdjf sldkfj sldkfj sldkfj  
+Technologies used:
+HTML,JavaScript,3rd Party API,SQL
+
+
+## Table of Contents
+
+* [Installation](#installation)
+* [Usage](#usage)
+* [Credits](#credits)
+* [License](#license)
+* [Contributing](#contributing)
+* [Tests](#tests)
+* [Questions](#questions)
+
+
+## Installation
+
+sldkjf slskdjf lskdjf sldkjf sldkfj 
+
+
+## Usage 
+
+lskdjf sldkjf lsdkjf sldkfj sldkfj  
+
+
+## Credits
+
+JB, Jaired, and friends 
+
+
+## License
+
+Mozilla Public License v2.0
+
+![GitHub license](https://img.shields.io/badge/license-Mozilla-blue.svg)
+
+
+## Contributing
+
+If you want to contribute, do this... . The [Contributor Covenant](https://www.contributor-covenant.org/) is an industry standard, but you can always write your own.
+
+
+## Tests
+
+
+
+       
+## Questions
+
+If you have any questions, please email JB Blot at: jb@motionmusic.us
+
+If you'd like to clone or explore the app repository, it is available on GitHub:
+https://github.com/jaaybe/
+    
--- Develop/index.js
+++ Develop/index.js
@@ -1,104 +1,88 @@
 const fs = require('fs');
 const inquirer = require('inquirer');
+const generatePage = require('./src/page-template');
 
-// array of questions for user
-// const questions = [
-
-  // Prompt the user
-  inquirer
-    .prompt([
-      {
-        type: 'input',
-        name: 'title',
-        message: 'What is the title of your web application?'
-      },
-      {
-        type: 'input',
-        name: 'Description',
-        message: 'Please enter a description of your application'
-      },
-      {
-        type: 'input',
-        name: 'Installation',
-        message: 'Please enter the installation instructions for your application'
-      },
-      {
-        type: 'input',
-        name: 'Usage',
-        message: 'Please enter a description of the usage for your application'
-      },
-      {
-        type: 'input',
-        name: 'Contributing',
-        message: 'Please enter your contribution guidelines'
-      },
-      {
-        type: 'input',
-        name: 'Tests',
-        message: 'Please enter the test instructions for your application'
-      },
-      {
-        type: 'checkbox',
-        message: 'What Technologies did you use?',
-        name: 'stack',
-        choices: ['HTML', 'CSS', 'JavaScript', '3rd Party API', 'JS Node', 'SQL']
-      },
-      {
-        type: 'list',
-        message: 'What license would you like to use for your application?',
-        name: 'License',
-        choices: ['Apache License 2.0', 'GNU General Public License v3.0', 'MIT License', 'BSD 2-Clause "Simplified" license'
-          , 'BSD 3-Clause "New" or "Revised" License', 'Boost Software License 1.0', 'Creative Commons Zero v1.0 Universal'
-          , 'Eclipse Public License 2.0', 'GNU Affero General Public License v3.0', 'GNU General Public License v2.0'
-          , 'GNU Lesser General Public License v2.1', 'Mozilla Public License v2.0', 'The Unlicense']
-      },
-      {
-        type: 'input',
-        name: 'GitHub',
-        message: 'What is your GitHub Username?'
-      },
-      {
-        type: 'input',
-        name: 'Email',
-        message: 'What is your Email Address?'
-      }
-    ])
-    // Write the user response to a file by chaining the below callback method to the prompt above.
-    .then(function (data) {
-      // Generate the name of your user file from their input
-      const filename =
-        README
-          // .split(' ')
-          .join('') + '.md';
-
-      fs.writeFile(filename, JSON.stringify(data, null, '\t'), function (err) {
-        if (err) {
-          return console.log(err);
+inquirer
+  .prompt([
+    {
+      type: 'input',
+      name: 'title',
+      message: 'What is the title of your web application? (Required)',
+      validate: function(title) {
+        if (title === '') {
+          console.log('\n Title is required.')
+          return false;
         }
 
-        console.log('Success!');
-      });
+        return true;
+      }
+    },
+    {
+      type: 'input',
+      name: 'description',
+      message: 'Please enter a description of your application'
+    },
+    {
+      type: 'input',
+      name: 'installation',
+      message: 'Please enter the installation instructions for your application'
+    },
+    {
+      type: 'input',
+      name: 'usage',
+      message: 'Please enter a description of the usage for your application'
+    },
+    {
+      type: 'input',
+      name: 'credits',
+      message: 'List any collabortors for your application'
+    },
+    {
+      type: 'list',
+      message: 'What license would you like to use for your application?',
+      name: 'license',
+      choices: ['Apache License 2.0', 'GNU General Public License v3.0', 'MIT License', 'BSD 2-Clause "Simplified" license'
+        , 'BSD 3-Clause "New" or "Revised" License', 'Boost Software License 1.0', 'Creative Commons Zero v1.0 Universal'
+        , 'Eclipse Public License 2.0', 'GNU Affero General Public License v3.0', 'GNU General Public License v2.0'
+        , 'GNU Lesser General Public License v2.1', 'Mozilla Public License v2.0', 'The Unlicense'],
+      default: 'MIT License'
+    },
+    {
+      type: 'input',
+      name: 'contributing',
+      message: 'Please enter your contribution guidelines'
+    },
+    {
+      type: 'input',
+      name: 'tests',
+      message: 'Please enter the test instructions for your application'
+    },
+    {
+      type: 'checkbox',
+      message: 'What Technologies did you use?',
+      name: 'tech',
+      choices: ['HTML', 'CSS', 'JavaScript', '3rd Party API', 'JS Node', 'SQL']
+    },
+    {
+      type: 'input',
+      name: 'github',
+      message: 'What is your GitHub Username?'
+    },
+    {
+      type: 'input',
+      name: 'email',
+      message: 'What is your Email Address?'
+    }
+  ])
+  
+
+  .then(function (answers) {
+    console.log(answers)
+    fs.writeFile('README.md', generatePage(answers), function (err) {
+      if (err) {
+        return console.log(err);
+      }
+
+      console.log('README file created!');
     });
-
-// ];
-
-// function to write README file
-// function writeToFile(fileName, data) {
-// }
-
-// function to initialize program
-// function init() {
-
-// }
-
-// function call to initialize program
-// init();
-
-
-
-
-
-
-
-
-
+  });
